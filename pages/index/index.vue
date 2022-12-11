08809133dd7072b281fe8b23ead6e53dc464cb5a
Minyoung <wlittleyang@163.com>
2022-12-11 22:09:52 +0800

    feat:wgt 在线更新

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,92 +1,85 @@
 <template>
-	<view class="content">
-		<view class="content-item">
-			<button @tap="downloadImage()">下载图片</button>
-			<view class="result-container">
-				<view
-					v-for="(item, key) in downloadResult"
-					:key="key"
-					class="result-item"
+	<view class='app-container'>
+		<FixedHeader />
+		<view>1</view>
+		<view>1</view>
+		<view>1</view>
+		<view>1</view>
+		<view>1</view>
+		<view>1</view>
+		<view>1</view>
+		<view>1</view>
+		<view>1</view>
+		<view>1</view>
+		<view class='menu'>
+			<view class='menu-item'>
+				<u-button
+					type='primary'
+					@tap='checkUpdate()'
 				>
-					<view>{{ key }}</view>
-					<view>{{ item }}</view>
-				</view>
+					检查更新
+				</u-button>
 			</view>
-		</view>
-		<view class="content-item">
-			<button @tap="getCacheImagesSize()">获取全部缓存文件大小</button>
-			<view class="result-container">
-				<view
-					v-for="(item, key) in sizeResult"
-					:key="key"
-					class="result-item"
+			<view class='menu-item'>
+				<u-button
+					type='primary'
+					@tap='upgradeApp()'
 				>
-					<view>{{ key }}</view>
-					<view>{{ item }}</view>
-				</view>
+					更新 APP
+				</u-button>
 			</view>
 		</view>
-		<view class="content-item">
-			<view
-				v-for="src in exampleUrls"
-				:key="src"
-				class="image-container"
-			>
-				<!-- <CacheImage
-					:src="src"
-					mode="widthFix"
-					class="full-image"
-				/> -->
-				<image :src="src" mode="widthFix" class="full-image"></image>
-				<text>{{ src }}</text>
-			</view>
-		</view>
-		<image v-if="downloadResult" :src="downloadResult.savedFilePath"></image>
 	</view>
 </template>
 
 <script>
-	import { CacheMixin } from './mixin';
-	import CacheImage from '@/components/CacheImage';
-	
+	import FixedHeader from '@/components/FixedHeader'
+	import { checkUpdate } from '@/utils/check-update';
 	export default {
-		name: 'Cache',
-		mixins: [CacheMixin],
+		name: 'homePage',
 		components: {
-			CacheImage
+			FixedHeader,
 		},
 		data() {
 			return {
-				title: 'Hello'
+				
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-
+			checkUpdate,
+			upgradeApp() {
+				const PACKAGE_INFO_KEY = '__package_info__';
+				const packageInfo = {
+					type: 'wgt',
+					'latestVersion': '1.0.1',
+					'name': 'common-examples',
+					'wgtUrl': '/wgt/common-examples_1.0.1.wgt'
+				}
+				uni.setStorageSync(PACKAGE_INFO_KEY, packageInfo);
+				uni.navigateTo({
+					url: `/pages/upgrade-center-app/upgrade-center-app?local_storage_key=${PACKAGE_INFO_KEY}`,
+					fail: (err) => {
+						console.error('更新弹框跳转失败', err)
+						uni.removeStorageSync(PACKAGE_INFO_KEY)
+					}
+				});
+			}
 		}
 	}
 </script>
 
-<style>
-	.content {
-		padding: 48upx;
-		box-sizing: border-box;
-	}
-	.result-item {
-		margin-top: 48upx;
-	}
-	.content-item {
-		margin-bottom: 48upx;
-	}
-	.full-image {
-		width: 100%;
-		display: block;
-		border-radius: 20upx;
-		box-shadow: 0 0 40upx rgba(0, 0, 0, .3);
-	}
-	.image-container ~ .image-container {
-		margin-top: 40upx;
-	}
+<style lang="scss" scoped>
+.app-container {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100vh;
+	padding-bottom: 83px;
+	box-sizing: border-box;
+	padding: 0 40upx;
+}
+.menu-item {
+	margin-bottom: 20upx;
+}
 </style>
---
+++ utils/check-update.js
@@ -0,0 +1,57 @@
+export const checkUpdate = () => {
+	// #ifdef APP-PLUS
+	plus.runtime.getProperty(plus.runtime.appid, function(widgetInfo) {
+		const baseURL = 'http://192.168.124.8:10086';
+		console.log('widgetInfo', widgetInfo);
+		uni.request({
+			url: `${baseURL}/check-update`,
+			data: {
+				version: widgetInfo.version,
+				name: widgetInfo.name
+			},
+			success(res) {
+				const { data } = res;
+				if (!data.haveUpdate) return;
+				const PACKAGE_INFO_KEY = '__package_info__';
+				const packageInfo = {
+					type: 'wgt',
+					...data,
+				}
+				uni.setStorageSync(PACKAGE_INFO_KEY, packageInfo);
+				uni.navigateTo({
+					url: `/pages/upgrade-center-app/upgrade-center-app?local_storage_key=${PACKAGE_INFO_KEY}`,
+					fail: (err) => {
+						console.error('更新弹框跳转失败', err)
+						uni.removeStorageSync(PACKAGE_INFO_KEY)
+					}
+				});
+			},
+			fail(err) {
+				console.error('下载失败', err);
+			}
+		});
+	});
+	// #endif
+};
+
+// 下载安装wgt 更包
+export const downloadUpgradePckage = (downloadUrl = '') => {
+	uni.downloadFile({
+		url: downloadUrl,
+		success(res) {
+			console.log('开始安装', res);
+			if (res.statusCode !== 200) return;
+			plus.runtime.install(res.tempFilePath, {
+				force: false
+			}, function() {
+				console.log('install success...');
+				plus.runtime.restart();
+			}, function(e) {
+				console.error('install fail...', e);
+			});
+		},
+		fail(err) {
+			console.log('下载失败', err);
+		}
+	});
+}
